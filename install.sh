b3b58db3012e14d70a4a3159bda295a9d6df8e9d
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2019-02-12 00:33:28 +0100

    add ssh_config

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
+ensure-folder() {
+  mkdir -p "$1" 2>&1 /dev/null
+}
+
+ensure-permissions() {
+  chmod -R "$1" "$2" 2>&1 /dev/null
+}
+
 symlink() {
+  local src
+  local target
+  src="$1"
+  target="$2"
   ln -sf "${HOME}/${1}" "${HOME}/${2}"
 }
 
+# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 symlink "dotfiles/zshrc" ".zshrc"
+
+# Install & Configure vim
 symlink "dotfiles/vimrc" ".vimrc"
+
+ensure-folder "${HOME}/.ssh"
+ensure-permissions "0600" "${HOME}/.ssh"
+symlink "dotfiles/ssh/config" ".ssh/config"
 
 # TODOs
 # Install software:
